1a50995ee01cff41656b367b14bdd8bd1c6021bf
ptkaczyk <45365849+ptkaczyk@users.noreply.github.com>
2020-09-23 10:50:26 -0400

    Add files via upload

---
+++ microblog/app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/landing')
+def landing():
+    return render_template('Landing.html')
+
+
+@app.route('/artistlist')
+def artistlist():
+
+    displayedartists = [
+        {'name': 'Jim and friends'}, {'name': 'People with Beards'}, {'name': 'No More Hats'},
+        {'name': 'The Musketeers'}
+    ]
+    return render_template('Artists.html', displayedartists=displayedartists)
+
+
+@app.route('/newartist')
+def newartist():
+    return render_template('NewArtist.html')
+
+
+@app.route('/artist')
+def artist():
+    return render_template('Artist.html')
