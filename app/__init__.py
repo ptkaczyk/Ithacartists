37c16676dc5a0e1988f2a8e347c419357a271835
ptkaczyk <45365849+ptkaczyk@users.noreply.github.com>
2020-11-23 20:14:47 -0500

    Lab 7

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,4 +13,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-from app import routes, models
+from app import routes, models, errors
+bootstrap = Bootstrap(app)
+#This is a init file
